f1d08144985fdbc7b33c6795520fe92fe77eab5c
Adam Grare <adam@grare.com>
2026-05-26 15:35:06 -0400

    Add Indexes for Container Inventory on EventStreams

--- spec/automated_review/foreign_key_columns_have_indexes_spec.rb
+++ spec/automated_review/foreign_key_columns_have_indexes_spec.rb
@@ -109,10 +109,6 @@ describe "foreign key (*_id) columns" do
       disks.backing_id
       disks.storage_profile_id
       entitlements.miq_user_role_id
-      event_streams.container_group_id
-      event_streams.container_id
-      event_streams.container_node_id
-      event_streams.container_replicator_id
       event_streams.dest_ems_cluster_id
       event_streams.group_id
       event_streams.target_id
